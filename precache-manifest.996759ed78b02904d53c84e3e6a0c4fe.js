007a988a942d71d0d662bbc04b9c1751ba914a93
Brendan <b.gibb@live.co.za>
2019-02-25 18:48:52 +0200

    Updates

---
+++ precache-manifest.996759ed78b02904d53c84e3e6a0c4fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b787a50361e518f366a52c458e1952d6",
+    "url": "/ReactChuckNorrisApp/static/media/background.b787a503.jpg"
+  },
+  {
+    "revision": "03d441e9e994d605889b",
+    "url": "/ReactChuckNorrisApp/static/js/runtime~main.03d441e9.js"
+  },
+  {
+    "revision": "a9acbfdc1163415c2382",
+    "url": "/ReactChuckNorrisApp/static/js/main.a9acbfdc.chunk.js"
+  },
+  {
+    "revision": "a858938ed61542b3a150",
+    "url": "/ReactChuckNorrisApp/static/js/2.a858938e.chunk.js"
+  },
+  {
+    "revision": "20d96725cdb9527407d9e820f6546edc",
+    "url": "/ReactChuckNorrisApp/index.html"
+  }
+];
